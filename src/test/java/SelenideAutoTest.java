92bbbd7f727fe02a17154f6ea60a27a8358c7246
Alex Nikiforov <unleash211@gmail.com>
2023-12-30 18:57:55 +0400

    HW download

---
+++ src/test/java/SelenideAutoTest.java
@@ -0,0 +1,33 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selectors.byText;
+
+public class SelenideAutoTest {
+
+    @BeforeAll
+    static void beforeAll (){
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void selenideHomework (){
+        open("https://github.com/selenide/selenide");
+        $$("ul.UnderlineNav-body li").get(5).click();
+        $(".markdown-body").$(byText("Soft assertions")).click();
+
+        $(".markdown-body").$(byText("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+    }
+}
